c583748e7d414d4524503d9816881fd39dca37bb
OmarAbdeldayem <omar.abdeldayem.personal@outlook.com>
2015-01-24 12:19:03 -0500

    Added manual control and mode switching

--- Rover_Arm_DefaultPositions/Rover_Arm_DefaultPositions.ino
+++ Rover_Arm_DefaultPositions/Rover_Arm_DefaultPositions.ino
@@ -7,7 +7,10 @@ enum positions {STANDARD,
                STORED,
                VERTICAL};
                
-int currentPosition, incomingByte, pos;
+int currentPosition, incomingByte;
+int wristPot, elbowPot, shoulderPot, basePot;
+int wristPotVal, elbowPotVal, shoulderPotVal, basePotVal;
+boolean automatic;
 
 void moveArm(int pos)
 {
@@ -47,33 +50,75 @@ void setup()
   shoulder.attach(28);
   base.attach(30);
   
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  
+  wristPot = 0;
+  elbowPot = 1;
+  shoulderPot = 2;
+  basePot = 3;
+  
+  automatic = true;
+  
   Serial.begin(9600);
 }
 
 void loop()
 {
-  if (Serial.available() > 0)
+  if (automatic)
   {
-    incomingByte = Serial.read();
-    Serial.print("I received: ");
-    Serial.println(incomingByte, DEC);  
-    switch (incomingByte)
+    if (Serial.available() > 0)
     {
-      case 49:
-        currentPosition = STANDARD;
-        break;
-      case 50:
-        currentPosition = READING;
-        break;
-      case 51:
-        currentPosition = STORED;
-        break;
-      case 52:
-        currentPosition = VERTICAL;
-        break;
-      default:
-        break;
+      incomingByte = Serial.read();
+      switch (incomingByte)
+      {
+        case 49:
+          currentPosition = STANDARD;
+          break;
+        case 50:
+          currentPosition = READING;
+          break;
+        case 51:
+          currentPosition = STORED;
+          break;
+        case 52:
+          currentPosition = VERTICAL;
+          break;
+        case 53: 
+          automatic = false;
+        default:
+          break;
+      }
+      moveArm(currentPosition);
     }
-    moveArm(currentPosition);
+  }
+  else 
+  {
+    elbowPotVal = analogRead(elbowPot);
+    shoulderPotVal = analogRead(shoulderPot);
+    wristPotVal = analogRead(wristPot);
+    basePotVal = analogRead(basePot);
+    
+    elbowPotVal = map(elbowPotVal, 0, 1023, 0, 179); 
+    shoulderPotVal = map(shoulderPotVal, 0, 1023, 0, 179);  
+    wristPotVal = map(wristPotVal, 0, 1023, 0, 179);
+    basePotVal = map(basePotVal, 0, 1023, 0, 179);
+    
+    elbow.write(elbowPotVal);
+    shoulder.write(shoulderPotVal);
+    wrist.write(wristPotVal);
+    base.write(basePotVal);
+    
+    if (Serial.available() > 0)
+    {
+      incomingByte = Serial.read();
+      if (incomingByte == 53)
+      {
+        automatic = true;
+      }
+    }
+    
   }
 }
