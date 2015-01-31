a8e36fa91040c0e38a091b6eb7d13ec9d938db2f
c2huc2hu <c-squared@sympatico.ca>
2015-01-31 18:11:37 -0500

    Fixed python files, added instructions to readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # RSX_RoverArm
 Code for controlling the rover's prototype arm.
+
+
+Instructions for Rover_Arm_DefaultPositions:
+ - Write a Python script that sends numbers 49 - 53 over the serial connection
+ - 49 through 52 correspond to preset positions
+ - 53 means manual control, so use the potentiometers (the knobs) to control the joints. 
+ 
+Instructions for Rover_Arm_SerialControl
+ - Plug in a joystick
+ - Run the Arduino file
+ - Run the python file
+ 
+ - Each axis on the joystick controls a joint. For the controllers that look like traditional gamecube controllers, the axes are the degrees of
+ freedom of the knobs (make sure the controller is set to analog, i.e. the red light is on). 
+ - The buttons (1-4) are the default positions. 
+ 
+ This file has more of the code in Python because it's easier than Arduino. 
+ 
--- Rover_Arm_SerialControl/rsx plastic arm.py
+++ Rover_Arm_SerialControl/rsx plastic arm.py
@@ -19,43 +19,54 @@ except Exception:
     raise Exception("Joystick not connected")
 clock = pygame.time.Clock()
 
+# INITIALIZE PRESET POSITIONS
 STANDARD = 0
 READING = 1
 STORED = 2
 VERTICAL = 3
 NUM_POSITIONS = 4
-
 preset_positions = {STANDARD : bytearray([255, 120, 135, 90]),
                     READING : bytearray([255, 90, 70, 90]),
-                    STORED : bytearray([255, 0, 180, 90]),
-                    VERTICAL : bytearray([255, 90, 180, 90])}
+                    STORED : bytearray([255, 5, 175, 90]),
+                    VERTICAL : bytearray([255, 90, 175, 90])}
+                    # 255 means "ignore me"
 
-joints = [90, 120, 135, 90]  # angles of each of the joints
+
+joints = [90, 120, 135, 90]  # set initial angles of each of the joints
 
 def to_bytearray(arr):
+    """
+    Convert arr, an array of floats or ints to a byte array 
+    """
     b = []
     for i in arr:
         b.append(int(i))
     return bytearray(b)
 
+# Main loop
 while(True):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
     pygame.event.pump()
-    
+
+    # Check the buttons to see if the user wants a preset position
     for i in range(NUM_POSITIONS):
         if joystick.get_button(i):
             # Use a preconfigured position
-            ser.write(str(preset_positions[i]))
-            for i in range(len(joints)):
-                joints[i] = int(preset_positions[i])
+            for j in range(len(joints)):
+                if preset_positions[i][j] != 255:  # 255 is the "ignore" flag
+                    joints[j] = preset_positions[i][j]
             break
 
-    move_joints = False # flag indicating whether any joints should be moved. 
+    # Check the joystick axes to see if the user is using them. 
     for i in range(4):
         if abs(joystick.get_axis(i)) > 0.1:  # eliminate noise
-            joints[i] += joystick.get_axis(i)
-            move_joints = True
+            joints[i] += joystick.get_axis(i) / 5
+        if 5 > joints[i]:
+            joints[i] = 5
+        if 175 < joints[i]:
+            joints[i] = 175
+    
     ser.write(str(to_bytearray(joints)))
-    print(to_bytearray(joints))
+    print(str(to_bytearray(joints)))
